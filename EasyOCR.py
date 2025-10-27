1ddd3292bd759e12844858f0022d5ca48030d3a4
Tabitha Davison <tdavison@olin.edu>
2025-10-27 19:03:15 -0400

    Easy OCR text detection and reading

---
+++ EasyOCR.py
@@ -0,0 +1,16 @@
+import easyocr, cv2
+
+easy_ocr_image =cv2.imread("easy_OCR.jpeg")
+
+reader = easyocr.Reader(['en'], gpu=True)
+easy_ocr_result = reader.readtext(easy_ocr_image, detail=1, paragraph = False)
+print(easy_ocr_result)
+
+for (coord, text, prob) in easy_ocr_result:
+  (top_left, top_right, bottom_right, bottom_left) = coord
+  tx, ty = (int(top_left[0]), int(top_left[1]))
+  bx, by = (int(bottom_right[0]), int(bottom_right[1]))
+  cv2.rectangle(easy_ocr_image, (tx, ty), (bx, by), (0, 255, 0), 2)
+  cv2.putText(easy_ocr_image, text, (tx, ty - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+cv2.imwrite("easy_ocr_result.png", easy_ocr_image)
