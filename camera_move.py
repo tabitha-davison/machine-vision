1fa366f17017c9dbc89aa5f87899e52a6a8a2cbf
Tabitha Davison <tdavison@olin.edu>
2025-11-02 17:55:47 -0500

    Very preliminary, with some info about what to do. Not much there.

---
+++ camera_move.py
@@ -0,0 +1,13 @@
+
+import cv2 as cv2
+import numpy as np
+
+"For this function, we need to use the angle of reflection formula and make sure that the angle of the raw of light and the phone angle are not parallel"
+"Can also pretend that we have a polarizing filter, so we can achieve Brewster's angle of perfect polarization"
+
+def camera_move(X_coord, S, ):
+
+"X-coordinate is the x,y for the center of the glare blob "
+
+X = X_coord
+
